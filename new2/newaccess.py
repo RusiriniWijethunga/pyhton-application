b7af87c071850367bd69b47fcc0163c367789124
RusiriniWijethunga <95708918+RusiriniWijethunga@users.noreply.github.com>
2024-11-07 22:06:43 +0530

    check new access #1

--- new2/newaccess.py
+++ new2/newaccess.py
@@ -1,2 +1,6 @@
 thislist = ["apple", "banana", "cherry"]
 print(thislist[-1])
+
+#new access check
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
