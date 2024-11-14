6c191e7345d800e9ec8a4e654695844d0da56459
RusiriniWijethunga <95708918+RusiriniWijethunga@users.noreply.github.com>
2024-11-15 00:39:22 +0530

    Remove item2

--- new2/remove.py
+++ new2/remove.py
@@ -5,3 +5,7 @@ print(thislist)
 thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
 thislist.remove("banana")
 print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
